2bc37a5c3b074db1c5523bb87abad8b165bc3a7d
trufas96 <rodrigolozano1996@gmail.com>
2018-02-19 16:14:35 +0100

    cuarto commit cambio delete y create terminados

--- fuel/app/classes/controller/Animals.php
+++ fuel/app/classes/controller/Animals.php
@@ -1,147 +1,197 @@
 <?php
-use \Model\Elements;
+use \Model\Animals;
 use Firebase\JWT\JWT;
 class Controller_Animals extends Controller_Base
 {
-	//private  $idAdmin = 1;
-    
-
-	public function post_create()
-	{
-	
-	$authenticated = $this->authenticate();
-    	$arrayAuthenticated = json_decode($authenticated, true);
-    	if($arrayAuthenticated['authenticated'])
-    	{
-    		$decodedToken = $this->decode($arrayAuthenticated['data']);
-    		if ($decodedToken->id == ID_ADMIN){
-			try {
-
-				if (!isset($_FILES['photo']) || empty($_FILES['photo'])) 
-		            {
-		            	$arrayData = array();
-		            	$arrayData['files'] = $_FILES;
-		            	$arrayData['post'] = $_POST; 
-		                $json = $this->response(array(
-		                    'code' => 400,
-		                    'message' => 'La photo esta vacia',
-		                    'data' =>  $arrayData
-		                ));
-		                return $json;
-		            }
-
-				if(isset($_POST['x']) || isset($_POST['y'])){
-				    if(empty($_POST['x']) || empty($_POST['y'])){
-					    return $this->respuesta(400, 'Coordenadas vacias', '');
-					}
-				}
-				else
-				{
-				    return $this->respuesta(400, 'Coordenadas no definidas', '');
-				}
-
-
-				
-				if ( !isset($_POST['name']) || !isset($_POST['description'])) 
-				{
-				    
-				
-					if(!empty($_POST['name']) && !empty($_POST['description']))
-					{
-					            	
-						$input = $_POST;
-						$newElement = $this->newElement($input);
-						$json = $this->saveElement($newElement);
-					}
-					else
-					{
-			    		return $this->respuesta(400, 'Algun campo vacio', '');
-					}
-				}
-			}
-
-	        $config = array(
-			    'path' => DOCROOT . 'assets/img',
-			    'randomize' => true,
-			    'ext_whitelist' => array('img', 'jpg', 'jpeg', 'gif', 'png'),
-			);
-
-			Upload::process($config);
-			$photo = "";
-			if (Upload::is_valid())
-			{
-			    Upload::save();
-			    foreach(Upload::get_files() as $file)
-			    {
-			            	
-			    $photo = 'http://localhost:8888/ARAPI/public/assets/img/'. $file['saved_as'];
-			            	
-				}
-			}
-
-	        foreach (Upload::get_errors() as $file)
-	        {
-			    return $this->response(array(
-			        'code' => 500,
-			        'message' => 'Error en el servidor',
-			        'data' => '' 
-			        ));
-	       }
+ 
+   
+ public function post_create()
+ {
+ 
+ $authenticated = $this->authenticate();
+     $arrayAuthenticated = json_decode($authenticated, true);
+     if($arrayAuthenticated['authenticated'])
+     {
+      $decodedToken = $this->decode($arrayAuthenticated['data']);
+      if ($decodedToken->id == ID_ADMIN){
+    try {
+     //photo
+     if (!isset($_FILES['photo']) || empty($_FILES['photo'])) 
+               {
+                $arrayData = array();
+                $arrayData['files'] = $_FILES;
+                $arrayData['post'] = $_POST; 
+                   $json = $this->response(array(
+                       'code' => 400,
+                       'message' => 'La photo esta vacia',
+                       'data' =>  $arrayData
+                   ));
+                   return $json;
+               }
+               //name
+               if(!isset($_POST['name']) || empty($_POST['name']))
+               {
+                $json = $this->response(array(
+                       'code' => 400,
+                       'message' => 'El nombre esta vacio',
+                       'data' => '' 
+                   ));
+                   return $json;
+               }
+               //description
+               if( !isset($_POST['description']) || empty($_POST['description']))
+               {
+                $json = $this->response(array(
+                       'code' => 400,
+                       'message' => 'La description esta vacio',
+                       'data' => '' 
+                   ));
+                   return $json;
+      }
+      //coordenadas
+               if(isset($_POST['x']) || isset($_POST['y'])){
+          if(empty($_POST['x']) || empty($_POST['y'])){
+          return $this->respuesta(400, 'Coordenadas vacias', '');
+        }
+      }
+      else
+      {
+          return $this->respuesta(400, 'Coordenadas no definidas', '');
+      }
 
 
-	       	$input = $_POST;
-			$newAnimal = $this->newAnimal($input, $photo, $decodedToken);
-			$json = $this->saveAnimal($newElement);
-		    return $json;
-
-
-			catch (Exception $e)
-			{
-				return $this->respuesta(500, $e->getMessage(), '');
-			}
-			}
-			else 
-			{
-				return $this->respuesta(400, 'No eres el admin', '');
-			}      
-		}
-	}
-
-	private function newAnimal($input)
-	{
-		$element = Model_Elements();
-		$element->name = $input['name'];
-		$element->description = $input['description'];
-		$element->photo = "";
-		$element->x = $input['x'];
-		$element->y = $input['y'];
-		$element->id_continent = $input['id_continent'];
-		$element->id_user = $this->idAdmin;
-		return $element;
-	}
-
-	private function saveAnimal($element)
+             $config = array(
+                'path' => DOCROOT . 'assets/img',
+                'randomize' => true,
+                'ext_whitelist' => array('img', 'jpg', 'jpeg', 'gif', 'png'),
+            );
+            Upload::process($config);
+            $photoToSave = "";
+            if (Upload::is_valid())
+            {
+                Upload::save();
+                foreach(Upload::get_files() as $file)
+                {
+                 $photoToSave = 'http://localhost:8888/CAMBIOAPI/public/assets/img/'. $file['saved_as'];
+                               
+                }
+            }
+            foreach (Upload::get_errors() as $file)
+            {
+                return $this->response(array(
+                    'code' => 500,
+                    'message' => 'Error en el servidor',
+                    'data' => '' 
+                ));
+            }
+        
+               $newStory = $this->newStory($_POST, $photoToSave, $decodedToken);
+               $json = $this->saveStory($newStory);
+               return $json;
+    catch (Exception $e)
     {
-    	$elementExists = Model_Elements::find('all', 
-    								array('where' => array(
-    													array('name', '=', $element->name),
-    														)
-    									)
-    							);
-    	if(empty($elementExists)){
-    		$elementToSave = $element;
-    		$elementToSave->save();
-    		$arrayData = array();
-    		$arrayData['name'] = $element->name;
-    		return $this->respuesta(201, 'Elemento creado', $arrayData);
-    	}else{
-    		return $this->respuesta(204, 'Elemento ya creado', '');
-    	}
+     return $this->respuesta(500, $e->getMessage(), '');
     }
+    }
+    else 
+    {
+     return $this->respuesta(400, 'No eres el admin', '');
+    }      
+   }
+  }
+ }
+public function post_delete()
+    {
+     $authenticated = $this->authenticate();
+     $arrayAuthenticated = json_decode($authenticated, true);
+     
+      if($arrayAuthenticated['authenticated']){
+       $decodedToken = JWT::decode($arrayAuthenticated["data"], MY_KEY, array('HS256'));
+       if(!empty($_POST['id'])){
+           $story = Model_Stories::find($_POST['id']);
+           if(isset($story)){
+            if($decodedToken->id == $story->id_user){
+             $story->delete(); 
+     
+             $json = $this->response(array(
+                 'code' => 200,
+                 'message' => 'animal borrado',
+                 'data' => ''
+             ));
+             return $json;
+            }
+            else
+            {
+             $json = $this->response(array(
+                 'code' => 401,
+                 'message' => 'No puede borrar un animal que no es tuyo',
+                 'data' => ''
+              ));
+              return $json;
+            }
+           }
+           else
+           {
+            $json = $this->response(array(
+                 'code' => 401,
+                 'message' => 'Animal no valido',
+                 'data' => ''
+              ));
+              return $json;
+            }
+          }
+          else
+          {
+            $json = $this->response(array(
+                'code' => 400,
+                'message' => 'El id no puede estar vacio',
+                'data' => ''
+       ));
+       return $json;
+       }
+         }
+         else
+         {
+           $json = $this->response(array(
+               'code' => 400,
+               'message' => 'Falta el autorizacion',
+               'data' => ''
+            ));
+            return $json;
+         }
+     }
 
-
-
-
-
-
+ private function newAnimal($input)
+ {
+  $animal = Model_Animals();
+  $animal->name = $input['name'];
+  $animal->description = $input['description'];
+  $animal->photo = "";
+  $animal->x = $input['x'];
+  $animal->y = $input['y'];
+  $animal->id_continent = $input['id_continent'];
+  $animal->id_user = $this-> ID_ADMIN;
+  return $animal;
+ }
+ private function saveAnimal($animal)
+    {
+     $animalExists = Model_Animals::find('all', 
+            array('where' => array(
+       array('name', '=', $animal->name),
+                  )
+       
+           );
+     if(empty($animalExists))
+     {
+      $animaltoSave = $animal;
+      $animaltoSave->save();
+      $arrayData = array();
+      $arrayData['name'] = $animal->name;
+      return $this->respuesta(201, 'Animal creado', $arrayData);
+     }
+     else
+     {
+      return $this->respuesta(204, 'Animal ya creado', '');
+     }
+    }
 }
--- fuel/app/classes/controller/Elements.php
+++ fuel/app/classes/controller/Elements.php
@@ -3,130 +3,200 @@ use \Model\Elements;
 use Firebase\JWT\JWT;
 class Controller_Elements extends Controller_Base
 {
-	private  $idAdmin = 1;
-    
+ private  $idAdmin = 1;
+   
+ public function post_create()
+ {
+ 
+ $authenticated = $this->authenticate();
+     $arrayAuthenticated = json_decode($authenticated, true);
+     if($arrayAuthenticated['authenticated'])
+     {
+      $decodedToken = $this->decode($arrayAuthenticated['data']);
+      if ($decodedToken->id == ID_ADMIN){
+   try {
 
-	public function post_create()
-	{
-	
-	$authenticated = $this->authenticate();
-    	$arrayAuthenticated = json_decode($authenticated, true);
-    	if($arrayAuthenticated['authenticated'])
-    	{
-    		$decodedToken = $this->decode($arrayAuthenticated['data']);
-    		if ($decodedToken->id == ID_ADMIN){
-			try {
+     //photo
+     if (!isset($_FILES['photo']) || empty($_FILES['photo'])) 
+               {
+                $arrayData = array();
+                $arrayData['files'] = $_FILES;
+                $arrayData['post'] = $_POST; 
+                   $json = $this->response(array(
+                       'code' => 400,
+                       'message' => 'La photo esta vacia',
+                       'data' =>  $arrayData
+                   ));
+                   return $json;
+               }
 
+               //name
+               if(!isset($_POST['name']) || empty($_POST['name']))
+               {
+                $json = $this->response(array(
+                       'code' => 400,
+                       'message' => 'El nombre esta vacio',
+                       'data' => '' 
+                   ));
+                   return $json;
+               }
 
-				if(isset($_POST['x']) || isset($_POST['y'])){
-				    if(empty($_POST['x']) || empty($_POST['y'])){
-					    return $this->respuesta(400, 'Coordenadas vacias', '');
-					}
-				}
-				else
-				{
-				    return $this->respuesta(400, 'Coordenadas no definidas', '');
-				}
+               //description
+               if( !isset($_POST['description']) || empty($_POST['description']))
+               {
+                $json = $this->response(array(
+                       'code' => 400,
+                       'message' => 'La description esta vacio',
+                       'data' => '' 
+                   ));
+                   return $json;
+      }
 
-
-				
-				if ( !isset($_POST['name']) || !isset($_POST['description'])) 
-				{
-				    
-				
-					if(!empty($_POST['name']) && !empty($_POST['description']))
-					{
-					            	
-						$input = $_POST;
-						$newElement = $this->newElement($input);
-						$json = $this->saveElement($newElement);
-					}
-					else
-					{
-			    		return $this->respuesta(400, 'Algun campo vacio', '');
-					}
-				}
-			}
-
-	        $config = array(
-			    'path' => DOCROOT . 'assets/img',
-			    'randomize' => true,
-			    'ext_whitelist' => array('img', 'jpg', 'jpeg', 'gif', 'png'),
-			);
-
-			Upload::process($config);
-			$photo = "";
-			if (Upload::is_valid())
-			{
-			    Upload::save();
-			    foreach(Upload::get_files() as $file)
-			    {
-			            	
-			    $photo = 'http://localhost:8888/ARAPI/public/assets/img/'. $file['saved_as'];
-			            	
-				}
-			}
-
-	        foreach (Upload::get_errors() as $file)
-	        {
-			    return $this->response(array(
-			        'code' => 500,
-			        'message' => 'Error en el servidor',
-			        'data' => '' 
-			        ));
-	       }
-
-	       
-	       	$input = $_POST;
-			$newElement = $this->newElement($input, $photo, $decodedToken);
-			$json = $this->saveElement($newElement);
-		    return $json;
+      //coordenadas
+               if(isset($_POST['x']) || isset($_POST['y'])){
+          if(empty($_POST['x']) || empty($_POST['y'])){
+          return $this->respuesta(400, 'Coordenadas vacias', '');
+        }
+      }
+      else
+      {
+          return $this->respuesta(400, 'Coordenadas no definidas', '');
+      }
 
 
-			catch (Exception $e)
-			{
-				return $this->respuesta(500, $e->getMessage(), '');
-			}
-			}
-			else 
-			{
-				return $this->respuesta(400, 'No eres el admin', '');
-			}      
-		}
-	}
+             $config = array(
+                'path' => DOCROOT . 'assets/img',
+                'randomize' => true,
+                'ext_whitelist' => array('img', 'jpg', 'jpeg', 'gif', 'png'),
+            );
+            Upload::process($config);
+            $photoToSave = "";
+            if (Upload::is_valid())
+            {
+                Upload::save();
+                foreach(Upload::get_files() as $file)
+                {
+                 $photoToSave = 'http://localhost:8888/CAMBIOAPI/public/assets/img/'. $file['saved_as'];
+                               
+                }
+            }
+            foreach (Upload::get_errors() as $file)
+            {
+                return $this->response(array(
+                    'code' => 500,
+                    'message' => 'Error en el servidor',
+                    'data' => '' 
+                ));
+            }
+        
+               $newStory = $this->newStory($_POST, $photoToSave, $decodedToken);
+               $json = $this->saveStory($newStory);
+               return $json;
 
-	private function newElement($input)
-	{
-		$element = Model_Elements();
-		$element->name = $input['name'];
-		$element->description = $input['description'];
-		$element->photo = "";
-		$element->x = $input['x'];
-		$element->y = $input['y'];
-		$element->id_type = $input['id_type'];
-		$element->id_user = $this->idAdmin;
-		return $element;
-	}
-
-	private function saveElement($element)
+   catch (Exception $e)
+   {
+    return $this->respuesta(500, $e->getMessage(), '');
+   }
+   
+   }
+   else 
+   {
+    return $this->respuesta(400, 'No eres el admin', '');
+   }      
+  }
+ }
+ private function newElement($input)
+ {
+  $element = Model_Elements();
+  $element->name = $input['name'];
+  $element->description = $input['description'];
+  $element->photo = "";
+  $element->x = $input['x'];
+  $element->y = $input['y'];
+  $element->id_type = $input['id_type'];
+  $element->id_user = $this-> ID_ADMIN;
+  return $element;
+ }
+ private function saveElement($element)
     {
-    	$elementExists = Model_Elements::find('all', 
-    								array('where' => array(
-    													array('name', '=', $element->name),
-    														)
-    									)
-    							);
-    	if(empty($elementExists)){
-    		$elementToSave = $element;
-    		$elementToSave->save();
-    		$arrayData = array();
-    		$arrayData['name'] = $element->name;
-    		return $this->respuesta(201, 'Elemento creado', $arrayData);
-    	}else{
-    		return $this->respuesta(204, 'Elemento ya creado', '');
-    	}
+     $elementExists = Model_Elements::find('all', 
+            array('where' => array(
+                 array('name', '=', $element->name),
+                  )
+             )
+           );
+     if(empty($elementExists))
+     {
+      $elementToSave = $element;
+      $elementToSave->save();
+      $arrayData = array();
+      $arrayData['name'] = $element->name;
+      return $this->respuesta(201, 'Elemento creado', $arrayData);
+     }
+     else
+     {
+      return $this->respuesta(204, 'Elemento ya creado', '');
+     }
     }
 
-
-
-}
+    public function post_delete()
+    {
+     $authenticated = $this->authenticate();
+     $arrayAuthenticated = json_decode($authenticated, true);
+     
+      if($arrayAuthenticated['authenticated']){
+       $decodedToken = JWT::decode($arrayAuthenticated["data"], MY_KEY, array('HS256'));
+       if(!empty($_POST['id'])){
+           $story = Model_Stories::find($_POST['id']);
+           if(isset($story)){
+            if($decodedToken->id == $story->id_user){
+             $story->delete(); 
+     
+             $json = $this->response(array(
+                 'code' => 200,
+                 'message' => 'elemento borrado',
+                 'data' => ''
+             ));
+             return $json;
+            }
+            else
+            {
+             $json = $this->response(array(
+                 'code' => 401,
+                 'message' => 'No puede borrar un elemento que no es tuyo',
+                 'data' => ''
+              ));
+              return $json;
+            }
+           }
+           else
+           {
+            $json = $this->response(array(
+                 'code' => 401,
+                 'message' => 'elemento no valido',
+                 'data' => ''
+              ));
+              return $json;
+            }
+          }
+          else
+          {
+            $json = $this->response(array(
+                'code' => 400,
+                'message' => 'El id no puede estar vacio',
+                'data' => ''
+       ));
+       return $json;
+       }
+         }
+         else
+         {
+           $json = $this->response(array(
+               'code' => 400,
+               'message' => 'Falta el autorizacion',
+               'data' => ''
+            ));
+            return $json;
+         }
+     }
+ }
