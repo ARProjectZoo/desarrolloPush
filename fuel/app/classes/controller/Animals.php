a082df0d3c8580054eaff5f636480cb87332c028
trufas96 <rodrigolozano1996@gmail.com>
2018-02-21 16:11:51 +0100

    cuarto commit cambio download probado perfectamente va postman

--- fuel/app/classes/controller/Animals.php
+++ fuel/app/classes/controller/Animals.php
@@ -4,8 +4,7 @@ use Firebase\JWT\JWT;
 class Controller_Animals extends Controller_Base
 {
  
-   
- public function post_create()
+   public function post_create()
  {
  
  $authenticated = $this->authenticate();
@@ -13,93 +12,113 @@ class Controller_Animals extends Controller_Base
      if($arrayAuthenticated['authenticated'])
      {
       $decodedToken = $this->decode($arrayAuthenticated['data']);
-      if ($decodedToken->id == ID_ADMIN){
-    try {
-     //photo
-     if (!isset($_FILES['photo']) || empty($_FILES['photo'])) 
-               {
-                $arrayData = array();
-                $arrayData['files'] = $_FILES;
-                $arrayData['post'] = $_POST; 
-                   $json = $this->response(array(
-                       'code' => 400,
-                       'message' => 'La photo esta vacia',
-                       'data' =>  $arrayData
-                   ));
-                   return $json;
-               }
-               //name
-               if(!isset($_POST['name']) || empty($_POST['name']))
-               {
-                $json = $this->response(array(
-                       'code' => 400,
-                       'message' => 'El nombre esta vacio',
-                       'data' => '' 
-                   ));
-                   return $json;
-               }
-               //description
-               if( !isset($_POST['description']) || empty($_POST['description']))
-               {
-                $json = $this->response(array(
-                       'code' => 400,
-                       'message' => 'La description esta vacio',
-                       'data' => '' 
-                   ));
-                   return $json;
-      }
-      //coordenadas
-               if(isset($_POST['x']) || isset($_POST['y'])){
-          if(empty($_POST['x']) || empty($_POST['y'])){
-          return $this->respuesta(400, 'Coordenadas vacias', '');
-        }
-      }
-      else
+      if ($decodedToken->id == ID_ADMIN)
       {
-          return $this->respuesta(400, 'Coordenadas no definidas', '');
-      }
+          try 
+          {
+
+             //photo
+             if (!isset($_FILES['photo']) || empty($_FILES['photo'])) 
+             {
+                        $arrayData = array();
+                        $arrayData['files'] = $_FILES;
+                        $arrayData['post'] = $_POST; 
+                           $json = $this->response(array(
+                               'code' => 400,
+                               'message' => 'La photo esta vacia',
+                               'data' =>  $arrayData
+                           ));
+                           return $json;
+              }
 
 
-             $config = array(
-                'path' => DOCROOT . 'assets/img',
-                'randomize' => true,
-                'ext_whitelist' => array('img', 'jpg', 'jpeg', 'gif', 'png'),
-            );
-            Upload::process($config);
-            $photoToSave = "";
-            if (Upload::is_valid())
-            {
-                Upload::save();
-                foreach(Upload::get_files() as $file)
-                {
-                 $photoToSave = 'http://localhost:8888/CAMBIOAPI/public/assets/img/'. $file['saved_as'];
-                               
-                }
-            }
-            foreach (Upload::get_errors() as $file)
-            {
-                return $this->response(array(
-                    'code' => 500,
-                    'message' => 'Error en el servidor',
-                    'data' => '' 
-                ));
-            }
-        
-               $newStory = $this->newStory($_POST, $photoToSave, $decodedToken);
-               $json = $this->saveStory($newStory);
-               return $json;
-    catch (Exception $e)
-    {
-     return $this->respuesta(500, $e->getMessage(), '');
-    }
-    }
-    else 
-    {
-     return $this->respuesta(400, 'No eres el admin', '');
-    }      
-   }
-  }
- }
+
+              //name
+              if(!isset($_POST['name']) || empty($_POST['name']))
+              {
+                        $json = $this->response(array(
+                               'code' => 400,
+                               'message' => 'El nombre esta vacio',
+                               'data' => '' 
+                           ));
+                           return $json;
+              }
+
+                       //description
+              if( !isset($_POST['description']) || empty($_POST['description']))
+              {
+                        $json = $this->response(array(
+                               'code' => 400,
+                               'message' => 'La description esta vacio',
+                               'data' => '' 
+                           ));
+                           return $json;
+              }
+
+
+
+
+              //coordenadas
+              if(isset($_POST['x']) || isset($_POST['y']))
+              {
+                              if(empty($_POST['x']) || empty($_POST['y']))
+                              {
+                              return $this->respuesta(400, 'Coordenadas vacias', '');
+                              }
+              }
+              else
+              {
+                              return $this->respuesta(400, 'Coordenadas no definidas', '');
+              }
+
+
+              $config = array(
+                          'path' => DOCROOT . 'assets/img',
+                          'randomize' => true,
+                          'ext_whitelist' => array('img', 'jpg', 'jpeg', 'gif', 'png'),
+              );
+              Upload::process($config);
+              $photoToSave = "";
+              if (Upload::is_valid())
+              {
+                          Upload::save();
+                          foreach(Upload::get_files() as $file)
+                          {
+                                $photoToSave = 'http://localhost:8888/CAMBIOAPI/public/assets/img/'. $file['saved_as'];
+                                         
+                          }
+              }
+              foreach (Upload::get_errors() as $file)
+              {
+                          return $this->response(array(
+                              'code' => 500,
+                              'message' => 'Error en el servidor',
+                              'data' => '' 
+                          ));
+              }
+                  
+                         $newStory = $this->newStory($_POST, $photoToSave, $decodedToken);
+                         $json = $this->saveStory($newStory);
+                         return $json;
+          }
+
+          catch (Exception $e)
+          {
+                         return $this->respuesta(500, $e->getMessage(), '');
+          }
+   
+        }
+        else 
+        {
+                return $this->respuesta(400, 'No eres el admin', '');
+        }
+    }     
+}
+
+ 
+
+
+
 
  //borrar
 public function post_delete()
@@ -198,6 +217,50 @@ public function post_delete()
         }
       }
 
+
+
+      public function get_download()
+    {
+
+        $authenticated = $this->authenticate();
+        $arrayAuthenticated = json_decode($authenticated, true);
+          if($arrayAuthenticated['authenticated'])
+          {
+          $decodedToken = $this->decode($arrayAuthenticated['data']);
+              if ($decodedToken->id != 1)
+              {
+
+              $animals = Model_Animals::find('all');
+                if(!empty($animals)){
+                  foreach ($animals as $key => $animal) {
+                    $arrayAnimals[] = $animal;
+                  }
+                  //$data = Arr::reindex($elements);
+                  $json = $this->response(array(
+                    'code' => 200,
+                    'message' => 'mostrando lista de elementos del usuario', 
+                    'data' => $arrayAnimals
+                    )); 
+                    return $json; 
+                }
+                else
+                {
+                  $json = $this->response(array(
+                    'code' => 202,
+                    'message' => 'Aun no tienes ningun elemento',
+                    'data' => ''
+                    ));
+                    return $json;
+                  }
+              }
+              else
+              {
+                return $this->respuesta(400, 'Eres el admin', '');
+              }
+          }
+    
+    }
+
       private function newAnimal($input)
       {
       $animal = Model_Animals();
@@ -217,7 +280,7 @@ public function post_delete()
            array('name', '=', $animal->name),
                       )
            
-               );
+               ));
         if(empty($animalExists))
         {
           $animaltoSave = $animal;
@@ -231,6 +294,8 @@ public function post_delete()
           return $this->respuesta(204, 'Animal ya creado', '');
         }
       }
+
+
 
 
 }
---
+++ fuel/app/logs/2018/02/21.php
@@ -0,0 +1,7 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2018-02-21 15:04:04 --> Error - syntax error, unexpected 'catch' (T_CATCH) in /Applications/MAMP/htdocs/CAMBIOAPI/fuel/app/classes/controller/animals.php on line 91
+ERROR - 2018-02-21 15:04:40 --> Error - syntax error, unexpected 'catch' (T_CATCH) in /Applications/MAMP/htdocs/CAMBIOAPI/fuel/app/classes/controller/animals.php on line 91
+ERROR - 2018-02-21 15:04:50 --> Error - syntax error, unexpected 'catch' (T_CATCH) in /Applications/MAMP/htdocs/CAMBIOAPI/fuel/app/classes/controller/animals.php on line 91
+ERROR - 2018-02-21 15:06:33 --> Error - syntax error, unexpected 'catch' (T_CATCH) in /Applications/MAMP/htdocs/CAMBIOAPI/fuel/app/classes/controller/animals.php on line 91
+ERROR - 2018-02-21 15:08:08 --> Error - syntax error, unexpected ';', expecting ',' or ')' in /Applications/MAMP/htdocs/CAMBIOAPI/fuel/app/classes/controller/animals.php on line 283
