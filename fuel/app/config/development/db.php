ac9327046d272201265c231fa49011cc3b370487
enriqueCea17 <enrique_ceano_vivas_apps1t1617@cev.com>
2018-03-14 19:43:09 +0100

    para subir

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -7,8 +7,8 @@ return array(
 	'default' => array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=zoo',
-			'username'   => 'root',
-			'password'   => 'root',
+			'username'   => 'zoo',
+			'password'   => 'XRSP6nF40HQe8jAi',
 		),
 	),
 );
